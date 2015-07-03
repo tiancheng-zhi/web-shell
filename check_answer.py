10da260cd2000a42c99cbe16a7d1139d466b7392
zhitiancheng <zhitiancheng1225@163.com>
2015-07-04 01:52:27 +0800

    update tutorial and check_answer

--- check_answer.py
+++ check_answer.py
@@ -1,13 +1,17 @@
 import re
 
+# class CheckAnswer is used to check whether the answer is correct
+
 class CheckAnswer:
 
+	# question number
 	__question_num = -1
 
 	def __init__(self, question_num):
 		self.__question_num = question_num
 		self.__stat = 0
 
+	# check the answer of question 0
 	def __check_func0(self, input_cmd, output_rst):
 		r = re.compile(r'^\s*echo\s+\\"Hello World!\\"\s*$')
 		if (r.match(input_cmd)):
@@ -15,6 +19,7 @@ class CheckAnswer:
 		else:
 			return False
 
+	# check the answer of question 1
 	def __check_func1(self, input_cmd, output_rst):
 		r = re.compile(r'^\s*./my.sh\s+apple\s+pear\s+peach\s*$')
 		if (r.match(input_cmd) and output_rst == 'pears'):
@@ -22,6 +27,7 @@ class CheckAnswer:
 		else:
 			return False
 
+	# check the answer of question 2
 	def __check_func2(self, input_cmd, output_rst):
 		r = re.compile(r'^\s*./month.sh\s+2004\s+2\s*$')
 		if (r.match(input_cmd) and output_rst == '29'):
@@ -29,13 +35,35 @@ class CheckAnswer:
 		else:
 			return False
 
+	# check the answer of question 3
+	def __check_func3(self, input_cmd, output_rst):
+		return False
+
+	# check the answer of question 4
+	def __check_func4(self, input_cmd, output_rst):
+		return False
+
+	# check the answer of question 5
+	def __check_func5(self, input_cmd, output_rst):
+		return False
+
+	# check the answer of question 6
+	def __check_func6(self, input_cmd, output_rst):
+		return False
+
+	# fucntion dictionary
 	__check_func = {
 					-1:None,
 					0: __check_func0,
 					1: __check_func1,
-					2: __check_func2
+					2: __check_func2,
+					3: __check_func3,
+					4: __check_func4,
+					5: __check_func5,
+					6: __check_func6
 					}
 
+	# check answer
 	def check_ans(self, input_cmd, output_rst):
 		return self.__check_func.get(self.__question_num)(self, input_cmd, output_rst.strip())
 

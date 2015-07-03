8cdc94435ea2186f5ae050328b905cdf66aa98ee
zhitiancheng <zhitiancheng1225@163.com>
2015-07-04 00:58:05 +0800

    update check_answer add 2.md

--- check_answer.py
+++ check_answer.py
@@ -17,7 +17,14 @@ class CheckAnswer:
 
 	def __check_func1(self, input_cmd, output_rst):
 		r = re.compile(r'^\s*./my.sh\s+apple\s+pear\s+peach\s*$')
-		if (r.match(input_cmd) and output_rst == 'pears\n'):
+		if (r.match(input_cmd) and output_rst == 'pears'):
+			return True
+		else:
+			return False
+
+	def __check_func2(self, input_cmd, output_rst):
+		r = re.compile(r'^\s*./month.sh\s+2004\s+2\s*$')
+		if (r.match(input_cmd) and output_rst == '29'):
 			return True
 		else:
 			return False
@@ -25,9 +32,11 @@ class CheckAnswer:
 	__check_func = {
 					-1:None,
 					0: __check_func0,
-					1: __check_func1
+					1: __check_func1,
+					2: __check_func2
 					}
 
 	def check_ans(self, input_cmd, output_rst):
-		return self.__check_func.get(self.__question_num)(self, input_cmd, output_rst)
+		return self.__check_func.get(self.__question_num)(self, input_cmd, output_rst.strip())
+
 
